a1e3f0261a085b6198dbadc1aaafcc2b48932207
Amar AA <amar.ashfaq95@gmail.com>
2025-06-18 14:51:17 +0100

    Implemented the GET endpoint for TrackStats

--- spotify-tracker-backend/src/routes/trackStats.ts
+++ spotify-tracker-backend/src/routes/trackStats.ts
@@ -65,4 +65,29 @@ app.post("/generate", async (request: Request, response: Response) => {
   }
 });
 
+// GET a single trackStat
+app.get("/", async (request: Request, response: Response) => {
+  const { type, period } = request.query;
+
+  if (!type || !period) {
+    response.status(400).json({ message: "Missing type or period" });
+  }
+
+  try {
+    const trackStat = await TrackStats.findOne({
+      type: type?.toString(),
+      period: period?.toString(),
+    });
+
+    if (!trackStat) {
+      response.status(404).json({ message: "No stats found" });
+      return;
+    }
+
+    response.json(trackStat);
+  } catch (error) {
+    response.status(500).json({ message: "Error fetching track", error });
+  }
+});
+
 export default app;
